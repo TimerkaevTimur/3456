48797f2fc2c49971df7f84516ba270a720e0bd4e
TimerkaevTimur <144235555+TimerkaevTimur@users.noreply.github.com>
2023-10-26 11:58:00 +0300

    Add files via upload

---
+++ klassy.py
@@ -0,0 +1,189 @@
+# 1
+#class Point3D:
+#def __init__(self, x, y, z):
+#self.x = x
+#self.y = y
+#self.z = z
+
+#def __str__(self):
+#return f"({self.x}, {self.y}, {self.z})"
+
+#point1 = Point3D(1, 2, 3)
+#point2 = Point3D(4, 5, 6)
+#point3 = Point3D(7, 8, 9)
+
+#print("Point 1:", point1)
+#print("Point 2:", point2)
+#print("Point 3:", point3)
+
+#point1.z = 10
+
+#print("\nПосле изменения класса Point3D:")
+#print("Point 1:", point1)
+#print("Point 2:", point2)
+#print("Point 3:", point3)
+
+#point2.x = 100
+
+#print("\nПосле изменения экземпляра Point3D (point2):")
+#print("Point 1:", point1)
+#print("Point 2:", point2)
+#print("Point 3:", point3)
+
+# 2
+#class Square:
+#def __init__(self, side_length):
+#self.side_length = side_length
+
+#def calculate_area(self):
+#return self.side_length ** 2
+
+#def calculate_perimeter(self):
+#return 4 * self.side_length
+
+#side_length = 5
+#square = Square(side_length)
+#area = square.calculate_area()
+#perimeter = square.calculate_perimeter()
+
+#print(f"Площадь квадрата: {area}")
+#print(f"Периметр квадрата: {perimeter}")
+
+# 3
+#import math
+
+#class Triangle:
+#def __init__(self, a, b):
+#self.a = a
+#self.b = b
+#self.hypotenuse = math.sqrt(a**2 + b**2)  # Вычисляем гипотенузу при создании объекта
+
+#def calculate_area(self):
+#area = 0.5 * self.a * self.b
+#return area
+
+#def calculate_perimeter(self):
+#perimeter = self.a + self.b + self.hypotenuse
+#return perimeter
+
+#triangle = Triangle(3, 5)
+
+#area = triangle.calculate_area()
+#perimeter = triangle.calculate_perimeter()
+
+#print(f"Площадь треугольника: {area}")
+#print(f"Периметр треугольника: {perimeter}")
+
+# 4
+#import math
+
+#class Point:
+#def __init__(self, x, y):
+#self.x = x
+#self.y = y
+
+#def distance_to(self, other_point):
+#return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+
+#class Triangle:
+#def __init__(self, point_a, point_b, point_c):
+#self.point_a = point_a
+#self.point_b = point_b
+#self.point_c = point_c
+
+#def perimeter(self):
+#side_ab = self.point_a.distance_to(self.point_b)
+#side_bc = self.point_b.distance_to(self.point_c)
+#side_ca = self.point_c.distance_to(self.point_a)
+#return side_ab + side_bc + side_ca
+
+#point_a = Point(2, 4)
+#point_b = Point(6, 9)
+#point_c = Point(6, 0)
+
+#triangle = Triangle(point_a, point_b, point_c)
+
+#perimeter = triangle.perimeter()
+#print("Периметр треугольника ABC:", perimeter)
+
+# 6
+# class CPerson:
+# def __init__(self, first_name, last_name, middle_name, birth_day, birth_month, birth_year, gender):
+# self.first_name = first_name
+# self.last_name = last_name
+# self.middle_name = middle_name
+# self.birth_day = birth_day
+# self.birth_month = birth_month
+# self.birth_year = birth_year
+# self.gender = gender
+#
+# def update_info(self, first_name, last_name, middle_name, birth_day, birth_month, birth_year, gender):
+# self.first_name = first_name
+# self.last_name = last_name
+# self.middle_name = middle_name
+# self.birth_day = birth_day
+# self.birth_month = birth_month
+# self.birth_year = birth_year
+# self.gender = gender
+#
+# def get_info(self):
+# return f"{self.last_name} {self.first_name} {self.middle_name}, Дата рождения: {self.birth_day}.{self.birth_month}.{self.birth_year}, Пол: {self.gender}"
+#
+# def __del__(self):
+# print(f"Объект {self.get_info()} удален")
+#
+# person1 = CPerson("Иван", "Иванов", "Иванович", 15, 7, 1990, "Мужской")
+# person2 = CPerson("Мария", "Петрова", "Сергеевна", 25, 4, 1985, "Женский")
+#
+# person1.update_info("Петр", "Сидоров", "Петрович", 10, 12, 1988, "Мужской")
+# print(person1.get_info())
+#
+# person2.update_info("Анна", "Смирнова", "Алексеевна", 5, 9, 1992, "Женский")
+# print(person2.get_info())
+#
+# class Phone:
+# def __init__(self, number, model, weight):
+# self.number = number
+# self.model = model
+# self.weight = weight
+#
+# def receive_call(self, caller_name):
+# print(f"Звонит {caller_name}")
+#
+# def get_number(self):
+# return self.number
+#
+# phone1 = Phone("+1234567890", "iPhone 13", 160)
+# phone2 = Phone("+9876543210", "Samsung Galaxy S21", 175)
+# phone3 = Phone("+1112233444", "Google Pixel 6", 150)
+#
+# print(f"Phone 1: Number - {phone1.number}, Model - {phone1.model}, Weight - {phone1.weight}")
+# print(f"Phone 2: Number - {phone2.number}, Model - {phone2.model}, Weight - {phone2.weight}")
+# print(f"Phone 3: Number - {phone3.number}, Model - {phone3.model}, Weight - {phone3.weight}")
+#
+# phone1.receive_call("John")
+# phone2.receive_call("Alice")
+# phone3.receive_call("Bob")
+#
+# print(f"Phone 1 number: {phone1.get_number()}")
+# print(f"Phone 2 number: {phone2.get_number()}")
+# print(f"Phone 3 number: {phone3.get_number()}")
+
+# 7
+# class Reader:
+# def __init__(self, full_name, ticket_number, faculty, birth_date, phone):
+# self.full_name = full_name
+# self.ticket_number = ticket_number
+# self.faculty = faculty
+# self.birth_date = birth_date
+# self.phone = phone
+#
+# def takeBook(self, book_count):
+# print(f"{self.full_name} взял {book_count} книги")
+#
+# def returnBook(self, book_count):
+# print(f"{self.full_name} вернул {book_count} книги")
+#
+# reader1 = Reader("Петров В. В.", "12345", "Информатика", "01.01.1990", "123-456-7890")
+# reader1.takeBook(3)
+# reader1.returnBook(2)
